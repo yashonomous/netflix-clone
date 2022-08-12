c79a439967954ca9bd2dc11b28e11d279ff920d1
yashonomous <yash.soni4@gmail.com>
2022-08-12 21:49:52 +0530

    first commit for netflix clone

---
+++ src/commons/tmdbApis.js
@@ -0,0 +1,15 @@
+const API_KEY = "0ba78b3e550268b4f715c3b89d02fac2";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+const URIs = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default URIs;
